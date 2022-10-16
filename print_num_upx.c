fab0a6df3c278aa5d1db88ebed75eae760e84802
dGunju <jonathanadegunju@gmail.com>
2022-10-16 15:27:59 +0000

    prints number in uppercase hex

--- print_num_upx.c
+++ print_num_upx.c
@@ -1,11 +1,11 @@
 #include "main.h"
- /**
-  * prinnupx - prints number in uppercase hex
-  * @arguments: input string
-  * @buf: buffer pointer
-  * @ibuf: index for buffer pointer
-  * Return: number of chars printed
-  */
+/**
+ * prinnupx - prints number in uppercase hex
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
 int prinnupx(va_list arguments, char *buf, unsigned int ibuf)
 {
 	int int_input, i, isnegative, count, first_digit;
